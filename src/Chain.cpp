c54759bc549e80c3559bf52335a79377509d08a4
aTxr <alexandre.tullot@gmail.com>
2021-09-07 19:40:04 +0200

    Fix bug frozen points

--- src/Chain.cpp
+++ src/Chain.cpp
@@ -22,9 +22,9 @@ void Chain::add_pt(Point pt)
 
 void Chain::update(float deltaTime)
 {
-    for (Point pt: m_pts)
+    for (int i=0; i<m_pts.size(); i++)
     {
-        pt.update(deltaTime);
+        m_pts[i].update(deltaTime);
     }
 
     for (int k=0; k<N_ITER; k++)
@@ -38,10 +38,10 @@ void Chain::update(float deltaTime)
 
 void Chain::draw(sf::RenderWindow *p_window)
 {
-    for (int i=0; i<SIZE; i++) {
-        m_pts[i].draw(p_window);
+    for (Point pt : m_pts) {
+        pt.draw(p_window);
     }
-    for (int i=0; i<SIZE-1; i++) {
-        m_sticks[i].draw(p_window);
+    for (Stick stick : m_sticks) {
+        stick.draw(p_window);
     }
 }
