8c0b39d489b336217a4533fd8a5bac8deaa93812
aTxr <alexandre.tullot@gmail.com>
2021-09-07 22:35:28 +0200

    Sketch of event management

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,9 @@ int main()
     chain.lock(0);
     chain.lock(5);
 
+    bool pause = true;
+    int mode = 0;
+
     sf::Clock deltaClock;
     float deltaTime(0.f);
 
@@ -23,16 +26,61 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch(event.type) { 
+                case(sf::Event::Closed):
+                    window.close();
+                    break;
 
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    chain.add_pt(Point(event.mouseButton.x, event.mouseButton.y));
-                    cout << "Point added" << endl;
-                }
+                case(sf::Event::MouseButtonPressed):
+                    switch(event.mouseButton.button) {
+                        case (sf::Mouse::Left):
+                            switch (mode) {
+                                case(0):
+                                    chain.add_pt(Point(event.mouseButton.x, event.mouseButton.y));
+                                    break;
+                                case(1):
+                                    // lock the selected Point
+                                    break;
+                                default:
+                                    // add stick
+                                    break;
+                            }
+                            break;
+
+                        case(sf::Mouse::Right):
+                            switch (mode) {
+                                case(0):
+                                    // delete a point
+                                    break;
+                                case(1):
+                                    // unlock the selected Point
+                                    break;
+                                default:
+                                    // remove stick
+                                    break;
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                case (sf::Event::KeyPressed):
+                    switch (event.key.code) {
+                        case (sf::Keyboard::Space):
+                            pause = !pause;
+                            break;
+                        case (sf::Keyboard::LControl):
+                            mode = mode + 1 % 3;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                default:
+                    break;
             }
         }
 
